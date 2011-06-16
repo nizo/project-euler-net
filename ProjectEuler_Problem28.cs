3d855ea867675173460705649e5135d610139dd1
Nizo <martin.niznansky@gmail.com>
2011-06-16 20:39:04 +0200

    Problem 28

---
+++ ProjectEuler_Problem28.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Diagnostics;
+
+namespace ProjectEuler_01
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			int lengthOfGrid = 1001;
+
+			int currentNumber = lengthOfGrid * lengthOfGrid;
+			int gridLength = lengthOfGrid - 1;
+			int modulatedCounter = 0;
+			int totalSum = 0;
+
+			while (currentNumber != 1)
+			{
+				if (modulatedCounter == 4)
+				{
+					modulatedCounter = 0;
+					gridLength -= 2;
+				}
+				modulatedCounter++;
+
+				totalSum += currentNumber;
+				currentNumber -= gridLength;
+			}
+
+			totalSum++;
+			System.Console.WriteLine(totalSum);
+			System.Console.ReadKey(true);
+		}
+	}
+}
+
+
+
+
+
