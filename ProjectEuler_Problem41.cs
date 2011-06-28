270113b9db5cb067bceb1ead420839a5fdb955cd
Nizo <martin.niznansky@gmail.com>
2011-06-29 00:37:38 +0200

    Problem 41 & 44

---
+++ ProjectEuler_Problem41.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication1
+{
+	class Program
+	{
+		static int maximum = 0;
+		static int maximumCurrentLength = 9;	
+
+		static bool IsNumberPrime(int i)
+		{
+			if (i == 2 || i == 3 || i == 5)
+				return true;
+			if (i % 2 == 0 || i % 3 == 0 || i % 5 == 0)
+				return false;
+
+			int limit = (int)Math.Sqrt(i);
+
+			for (int j = 7; j < limit; j += 2)
+				if (i % j == 0)
+					return false;
+
+			return true;
+		}
+
+		static void GetHighestPandigitalNumber(string number, int length)
+		{
+			if (length == maximumCurrentLength)
+			{
+				int candidate = int.Parse(number);				
+				if (IsNumberPrime(candidate) && (candidate > maximum))
+					maximum = candidate;					
+			}
+			else for (int i = 1; i <= maximumCurrentLength; i++)
+			{
+				if (!number.Contains(i.ToString()))
+					GetHighestPandigitalNumber(number + i.ToString(), length + 1);
+			}
+		}
+
+		static void Main(string[] args)
+		{
+			bool stop = false;
+
+			while (!stop)
+			{
+				GetHighestPandigitalNumber("", 0);
+				maximumCurrentLength--;
+				if (maximumCurrentLength == 0)
+					stop = true;
+			}
+
+			System.Console.WriteLine("Maximum such number is " + maximum);
+			System.Console.ReadKey(true);
+		}
+	}
+}
---
+++ ProjectEuler_Problem44.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication1
+{
+	class Program
+	{
+		static int limit = 50000;
+
+		static Dictionary<long, int> pentagonalNumbersDictionary = new Dictionary<long, int>();
+		static List<long> pentagonalNumbersList = new List<long>();
+
+		static void Main(string[] args)
+		{
+			long minimum = int.MaxValue;
+
+			for (int i = 0; i < limit; i++)
+			{
+				long number = (i * (3 * i - 1) / 2);
+				pentagonalNumbersDictionary.Add(number, (int)i);
+				pentagonalNumbersList.Add(number);
+			}
+
+
+			for (int i = 1; i < limit; i++)
+				for (int j = i; j < limit; j++)
+					if (pentagonalNumbersDictionary.ContainsKey(Math.Abs(pentagonalNumbersList[i] - pentagonalNumbersList[j])))
+						if (pentagonalNumbersDictionary.ContainsKey(pentagonalNumbersList[i] + pentagonalNumbersList[j]))
+							if (Math.Abs(pentagonalNumbersList[i] - pentagonalNumbersList[j]) < minimum && i != j)
+								minimum = Math.Abs(pentagonalNumbersList[i] - pentagonalNumbersList[j]);
+
+			System.Console.WriteLine("Minimalised difference is " + minimum);
+			System.Console.ReadKey(true);
+		}
+	}
+}
