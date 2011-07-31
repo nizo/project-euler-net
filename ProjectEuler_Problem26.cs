51b305abdd59b60dbe24806e87e978ff51cf0cac
Nizo <martin.niznansky@gmail.com>
2011-07-31 19:33:05 +0200

    Problem 26 & 54

---
+++ ProjectEuler_Problem26.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Diagnostics;
+using Oyster.Math;
+
+namespace ProjectEuler_01
+{
+	class Program
+	{
+
+		static int[] numberArray = new int[10];
+
+		static int Factorial(int i)
+		{
+			if (i <= 1)
+				return 1;
+			else
+				return i * (Factorial(i - 1));
+		}
+
+		static bool IsNumberPrime(int i)
+		{
+			int limit = (int)Math.Sqrt(i);
+
+			if (i == 2 || i == 3 || i == 5)
+				return true;
+
+			if (i % 2 == 0 || i % 3 == 0 || i % 5 == 0)
+				return false;
+
+			for (int j = 3; j <= limit; j += 2)
+			{
+				if (i % j == 0)
+					return false;
+			}
+			return true;
+		}
+
+		static bool IsNumberPrimitiveRoot(long prime)
+		{
+			List<long> factors = GetFactors(prime - 1);
+
+			for (int i = 1; i < factors.Count; i++)
+			{
+				long localResult = (long)Math.Pow(10, (prime - 1) / factors[i]);
+				if (localResult % prime == 0)
+					return true;
+			}
+			return true;
+		}
+
+		static List<long> GetFactors(long i)
+		{
+			List<long> result = new List<long>();
+			int currentFactor = 2;
+			bool doIt = true;
+
+			while (doIt)
+			{
+				if (i == 1)
+				{
+					doIt = false;
+					continue;
+				};
+
+				if (i % currentFactor == 0)
+				{
+					i /= currentFactor;
+					result.Add(currentFactor);
+					currentFactor = 2;
+					continue;
+				}
+
+				currentFactor++;
+			}
+
+			return result;
+		}
+
+		static void LongestRecurringCycle(int i)
+		{
+			for (int j = 3; j < i; j += 2)
+				if (IsNumberPrime(j) && IsNumberPrimitiveRoot(j))
+					System.Console.WriteLine("Prime with root factor 10 is " + j);
+		}
+
+		static void Main(string[] args)
+		{
+			Stopwatch sw = new Stopwatch();
+			sw.Start();
+
+			LongestRecurringCycle(1000);
+
+			//Console.WriteLine(IsNumberPrimitiveRoot(29));
+
+			sw.Stop();
+			Console.WriteLine(sw.Elapsed);
+			Console.ReadLine();
+
+		}
+	}
+}
---
+++ ProjectEuler_Problem54.cs
@@ -0,0 +1,247 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Diagnostics;
+using Oyster.Math;
+
+namespace ProjectEuler_01
+{
+	class Program
+	{
+		public static Stopwatch sw;
+		enum PokerDeals : int { HighCard, OnePair, TwoPairs, ThreeOfAKind, Straight, Flush, FullHouse, FourOfAKind, StraightFlush, RoyalFlush };
+
+		static PokerDeals GetPokerDeal(string s, out int highestCard, out int highestCardInHand)
+		{
+			int countOfSameCards = 1;
+			int currentHighestCard = 0;
+			int highestCardInHandSaved = 0;
+			bool[] usedCards = new bool[10];
+			List<int> advanceCards = new List<int>();
+			highestCardInHand = 0;
+			highestCard = 0;
+			string advanceString = "";
+
+			bool onePair = false;
+			bool twoPairs = false;
+			bool threeOfAKind = false;
+			bool straight = true;
+			bool flush = true;
+			bool fourOfAKind = false;
+			bool possibleRoyalFlush = false;
+
+			s = s.Replace(" ", "");
+
+			for (int i = 0; i < s.Length - 2; i += 2)  // Pairs, tripples, quarters, full house
+			{
+				for (int j = i + 2; j < s.Length; j += 2)
+				{
+					if (i == j) continue;
+
+					if (!usedCards[j] && s[i] == s[j])
+					{
+						usedCards[j] = true;
+						usedCards[i] = true;
+
+						countOfSameCards++;
+						advanceString = s.Substring(j, 1);
+						advanceString = advanceString.Replace("T", "10");
+						advanceString = advanceString.Replace("J", "11");
+						advanceString = advanceString.Replace("Q", "12");
+						advanceString = advanceString.Replace("K", "13");
+						advanceString = advanceString.Replace("A", "14");
+
+						if (int.Parse(advanceString) > currentHighestCard)
+							currentHighestCard = int.Parse(advanceString);
+					}
+				}
+
+				if (countOfSameCards == 2)
+				{					
+					if (onePair)
+						twoPairs = true;
+					onePair = true;
+
+					if (!threeOfAKind)
+						highestCard = currentHighestCard;
+
+					for (int j = 0; j < s.Length; j += 2)
+					{
+						if (!usedCards[j])
+						{
+							advanceString = s.Substring(j, 1);
+							advanceString = advanceString.Replace("T", "10");
+							advanceString = advanceString.Replace("J", "11");
+							advanceString = advanceString.Replace("Q", "12");
+							advanceString = advanceString.Replace("K", "13");
+							advanceString = advanceString.Replace("A", "14");
+
+							if (highestCardInHandSaved < int.Parse(advanceString))
+								highestCardInHandSaved = int.Parse(advanceString);
+						}
+					}
+				}
+
+				if (countOfSameCards == 3)
+				{
+					threeOfAKind = true;
+					highestCard = currentHighestCard;
+				}
+
+				if (countOfSameCards == 4)
+				{
+					highestCard = currentHighestCard;
+					fourOfAKind = true;
+				}
+				
+
+				countOfSameCards = 1;
+				currentHighestCard = 0;
+			}
+			
+			for (int i = 0; i < s.Length; i += 2)
+			{				
+				advanceString = s.Substring(i, 1);
+				advanceString = advanceString.Replace("T", "10");
+				advanceString = advanceString.Replace("J", "11");
+				advanceString = advanceString.Replace("Q", "12");
+				advanceString = advanceString.Replace("K", "13");
+				advanceString = advanceString.Replace("A", "14");
+
+				if (highestCardInHand < int.Parse(advanceString))				
+					highestCardInHand = int.Parse(advanceString);
+				
+				if (advanceString == "14")
+					possibleRoyalFlush = true;
+				
+				advanceCards.Add(int.Parse(advanceString));
+			}
+			
+			advanceCards.Sort();
+			int currentNumber = advanceCards[0];
+
+			for (int i = 0; i < 5; i++)
+			{
+				if (advanceCards[i] != currentNumber)
+					straight = false;
+
+				currentNumber++;
+			}
+			if (straight)
+				highestCard = advanceCards[4];
+
+			for (int i = 1; i < s.Length; i += 2)
+			{
+				advanceString = s.Substring(i - 1, 1);
+				advanceString = advanceString.Replace("T", "10");
+				advanceString = advanceString.Replace("J", "11");
+				advanceString = advanceString.Replace("Q", "12");
+				advanceString = advanceString.Replace("K", "13");
+				advanceString = advanceString.Replace("A", "14");
+
+				if (currentHighestCard < int.Parse(advanceString))
+					currentHighestCard = int.Parse(advanceString);
+				
+				if (s[1] != s[i])
+					flush = false;
+			}
+
+			if (flush)
+				highestCard = currentHighestCard;
+
+			if (flush && straight && possibleRoyalFlush)			
+				return PokerDeals.RoyalFlush;
+			if (flush && straight)
+				return PokerDeals.StraightFlush;
+			if (fourOfAKind)			
+				return PokerDeals.FourOfAKind;			
+			if (onePair && threeOfAKind)			
+				return PokerDeals.FullHouse;
+			if (flush)			
+				return PokerDeals.Flush;
+			if (straight)			
+				return PokerDeals.Straight;
+			if (threeOfAKind)
+				return PokerDeals.ThreeOfAKind;
+			if (twoPairs)
+				return PokerDeals.TwoPairs;
+			if (onePair)
+			{
+				highestCardInHand = highestCardInHandSaved;
+				return PokerDeals.OnePair;
+			}
+
+			return PokerDeals.HighCard;
+		}
+
+		static void Main(string[] args)
+		{
+			StartingProcedures();
+
+			PokerDeals playerOneHand = new PokerDeals();
+			PokerDeals playerTwoHand = new PokerDeals();
+			int playerOneHighestCard = 0;
+			int playerTwoHighestCard = 0;
+			int playerOneHighestCardInHand = 0;
+			int playerTwoHighestCardInHand = 0;
+			int playerOneWins = 0;
+			int playerTwoWins = 0;
+
+			using (StreamReader streamReader = new StreamReader("poker.txt"))
+			{
+				string line;
+				while ((line = streamReader.ReadLine()) != null)
+				{
+					playerOneHand = GetPokerDeal(line.Substring(0, 14), out playerOneHighestCard, out playerOneHighestCardInHand);
+					playerTwoHand = GetPokerDeal(line.Substring(14, 15), out playerTwoHighestCard, out playerTwoHighestCardInHand);					
+
+					if (playerOneHand == playerTwoHand)
+					{
+						if (playerOneHighestCard > playerTwoHighestCard)
+							playerOneWins++;
+						else if (playerOneHighestCard < playerTwoHighestCard)
+							playerTwoWins++;
+						else
+						{
+							if (playerOneHighestCardInHand < playerTwoHighestCardInHand)
+								playerTwoWins++;
+							else
+								playerOneWins++;
+						}
+
+					}
+					else if (playerOneHand > playerTwoHand)					
+						playerOneWins++;
+					else
+						playerTwoWins++;
+					
+				}				
+				streamReader.Close();
+			}
+			
+			System.Console.WriteLine("Player one won " + playerOneWins.ToString() + " tables");								
+			
+			ClosingProcedures();
+		}
+
+		#region HelpMethods
+
+		static void StartingProcedures()
+		{
+			sw = new Stopwatch();
+			sw.Start();
+		}
+
+		static void ClosingProcedures()
+		{
+			sw.Stop();
+			Console.WriteLine(sw.Elapsed);
+			Console.ReadLine();
+		}
+
+		#endregion
+
+	}
+}
